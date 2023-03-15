d260d445e1a7b7300f0a10a6a67178a6336a15d0
Aditya <adityakr365@gmail.com>
2023-03-15 14:30:00 +0530

    adding datatypes logic

---
+++ javascriptlogics/datatype.js
@@ -0,0 +1,9 @@
+let greeting = "Hello, World!";
+let age = 25;
+let isStudent = true;
+console.log(greeting," ",age,isStudent)
+console.log(typeof(age),typeof(isStudent),typeof(greeting))
+const pi = 3.14;
+let score = 100;
+score = score + 10;
+pi=pi+10;
